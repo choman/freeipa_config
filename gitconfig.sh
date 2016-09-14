92df2bee44bdced519fbd7c983bd329413793946
Chad Homan <choman@gmail.com>
2016-09-14 15:15:34 -0500

    Updated gitconfig for specific

--- gitconfig.sh
+++ gitconfig.sh
@@ -10,3 +10,5 @@ git config --local user.name "$NAME"
 git config --local user.email "$EMAIL"
 git config --local credential.helper cache
 
+git config remote.origin.url https://choman@github.com/choman/freeipa_config.git
+
